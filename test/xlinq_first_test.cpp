885256b55745dd7b9d5eaf46512c48f15772a8c1
TrolleY <trolleyyy@github.com>
2017-09-12 22:42:07 +0200

    first with predicate

--- test/xlinq_first_test.cpp
+++ test/xlinq_first_test.cpp
@@ -15,12 +15,47 @@ TEST(XlinqFirstTest, GetFirstElementOfCollection)
 	ASSERT_EQ(1, val);
 }
 
-TEST(XlinqFirstTest, CollectionContainsNoLements)
+TEST(XlinqFirstTest, CollectionContainsNoElements)
 {
 	std::vector<int> numbers;
 	try
 	{
 		from(numbers) >> first();
+		FAIL();
+	}
+	catch (IterationFinishedException exc)
+	{
+
+	}
+}
+
+TEST(XlinqFirstPredicateTest, GetFirstElementOfCollection)
+{
+	auto numbers = { 1, 2, 3, 4, 5 };
+	auto val = from(numbers) >> first([](int a) { return a > 2; });
+	ASSERT_EQ(3, val);
+}
+
+TEST(XlinqFirstPredicateTest, CollectionContainsNoElements)
+{
+	std::vector<int> numbers;
+	try
+	{
+		from(numbers) >> first([](int a) { return a > 2; });
+		FAIL();
+	}
+	catch (IterationFinishedException exc)
+	{
+
+	}
+}
+
+TEST(XlinqFirstPredicateTest, NoElementSatisfiesCondition)
+{
+	auto numbers = { 1, 2, 3, 4, 5 };
+	try
+	{
+		from(numbers) >> first([](int a) { return a > 10; });
 		FAIL();
 	}
 	catch (IterationFinishedException exc)
@@ -36,9 +71,30 @@ TEST(XlinqFirstOrDefaultTest, GetFirstElementOfCollection)
 	ASSERT_EQ(1, val);
 }
 
-TEST(XlinqFirstOrDefaultTest, CollectionContainsNoLements)
+TEST(XlinqFirstOrDefaultTest, CollectionContainsNoElements)
 {
 	std::vector<int> numbers;
 	auto val = from(numbers) >> first_or_default(0);
+	ASSERT_EQ(0, val);
+}
+
+TEST(XlinqFirstOrDefaultPredicateTest, GetFirstElementOfCollection)
+{
+	auto numbers = { 1, 2, 3, 4, 5 };
+	auto val = from(numbers) >> first_or_default(0, [](int a) { return a > 2; });
+	ASSERT_EQ(3, val);
+}
+
+TEST(XlinqFirstOrDefaultPredicateTest, CollectionContainsNoElements)
+{
+	std::vector<int> numbers;
+	auto val = from(numbers) >> first_or_default(0, [](int a) { return a > 2; });
+	ASSERT_EQ(0, val);
+}
+
+TEST(XlinqFirstOrDefaultPredicateTest, NoElementSatisfiesCondition)
+{
+	auto numbers = { 1, 2, 3, 4, 5 };
+	auto val = from(numbers) >> first_or_default(0, [](int a) { return a > 10; });
 	ASSERT_EQ(0, val);
 }
